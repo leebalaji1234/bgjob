67607ec51de8b353d7dba08193780868e934a4f2
leebalaji1234 <leebalaji@gmail.com>
2015-04-17 13:32:10 +0530

    accept Parameters filter added

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -1,18 +1,26 @@
 class BasicController < ActionController::Base
-	# ApplicationController
-	def index
+
+      # ApplicationController
+      def index 
+            @GatewayParams = filer_params     
+            @tempParams = RequestReceiverTemp.new
+            @tempParams.params = @GatewayParams.to_s
+            @tempParams.status = 0  # ready to process
+            @tempParams.save
+            render :text => "Request received successfully" 
+      end
+      def create
       # raise params.inspect
-      @tempParams = RequestReceiverTemp.new
-      @tempParams.params = params.to_s
-      @tempParams.status = 0  # ready to process
-      @tempParams.save
-      render :text => "Request received successfully" 
-	end
-	def create
-      # raise params.inspect
-      @tempParams = RequestReceiverTemp.new
-      @tempParams.params = params.to_s
-      @tempParams.status = 0  # ready to process
-      @tempParams.save
-	end
+            @tempParams = RequestReceiverTemp.new
+            @tempParams.params = params.to_s
+            @tempParams.status = 0  # ready to process
+            @tempParams.save
+      end
+      private
+      def filer_params
+            objParams = {}
+            objParams['callerid'] = params[:callerid]   
+            return objParams
+      end 
+
 end
